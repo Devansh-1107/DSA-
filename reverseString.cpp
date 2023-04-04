7b6fc6f9202dadc62aa34ec65d40a8c3f4d0ff3f
Devansh Pandey <85013112+Devansh-1107@users.noreply.github.com>
2023-04-05 02:11:36 +0530

    Add files via upload

---
+++ reverseString.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int size(char ch[])
+{
+    int count=0;
+    for(int i=0; ch[i]!='\0';i++)
+    {
+        count++;
+    }
+    return count;
+}
+void reverseString(char ch[])
+{
+    int s=0;
+    int e=size(ch)-1;
+    while(s<=e)
+    {
+        swap(ch[s],ch[e]);
+        s++;
+        e--;
+    }
+    cout<<"The reversed string is "<<ch;
+}
+int main ()
+{
+    char ch[100];
+    cout<<"Enter the string to be reversed--";
+    cin>>ch;
+    reverseString(ch);
+    return 0;
+}
