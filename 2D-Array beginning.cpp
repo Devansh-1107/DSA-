844d80cad97a7df323edb7efe1c61c46ccdeab02
Devansh Pandey <85013112+Devansh-1107@users.noreply.github.com>
2023-04-08 22:37:38 +0530

    Add files via upload

---
+++ 2D-Array beginning.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[3][4];
+    //for initialising rowwise
+    //arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    // taking input in 2-D array row-wise
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+     // taking input in 2-D array column-wise
+    // for(int i=0;i<4;i++)
+    // {
+    //     for(int j=0;j<3;j++)
+    //     {
+    //         cin>>arr[j][i];
+    //     }
+    // }
+
+    // printing the array
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}   
