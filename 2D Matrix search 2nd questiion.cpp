3a8f3f6b92b3bd3b657c35e6f67347a636c4efd8
Devansh Pandey <85013112+Devansh-1107@users.noreply.github.com>
2023-04-13 00:32:18 +0530

    Add files via upload

---
+++ 2D Matrix search 2nd questiion.cpp
@@ -0,0 +1,34 @@
+//in this question all elements in the row and column of the 2D array are already sorted 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    bool searchMatrix(vector<vector<int>>&matrix, int target)
+    {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int rowIndex=0;
+        int colIndex=col-1;
+        // search is being started from the top right corner
+        
+        while(rowIndex<row && colIndex>=0)
+        {
+            int element= matrix[rowIndex][colIndex];
+            if(element == target)
+            {
+                return 1;
+            }
+            else if(element < target)
+            {
+                rowIndex--;
+            }
+            else
+            {
+                colIndex++;
+            }
+        }
+        return 0;
+    }
+};
