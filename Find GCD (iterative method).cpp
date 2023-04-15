15f7bab194dd7a0251b763d2e8085f02551c0ef7
Devansh Pandey <85013112+Devansh-1107@users.noreply.github.com>
2023-04-16 00:20:05 +0530

    Add files via upload

---
+++ Find GCD (iterative method).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int GCD(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    else if (b == 0)
+    {
+        return a;
+    }
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a -= b;
+        }
+        else
+        {
+            b -= a;
+        }
+    }
+    return a;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "The GCD of the given input is " << GCD(a, b);
+    return 0;
+}
