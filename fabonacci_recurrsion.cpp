1d77ca1f41767e7ddeb40baa40da5baa47e11610
Devansh Pandey <pandeydevansh5690@gmail.com>
2024-02-24 19:16:14 +0530

    Add files via upload

---
+++ fabonacci_recurrsion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fabonacci(int n){
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+    return fabonacci(n-1)+ fabonacci(n-2);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fabonacci(n);
+    return 0;
+}
